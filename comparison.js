4a2bdce55feb97b713d95e6c286ab5df8f1a20d4
Samuel Rodríguez <espeon.elgatocosmico@gmail.com>
2018-07-18 00:55:00 +0200

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,7 @@
+var hungerLevel = 10;
+
+if (hungerLevel > 7) { // we can also use =>, <=, !==, ===, etc. for comparing a variable's value to a set condition
+  console.log('Time to eat!');
+} else {
+  console.log('Let\'s eat later.');
+}
