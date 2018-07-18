b1bf5916a7d93a529584705c8862147c699a2558
Samuel Rodríguez <espeon.elgatocosmico@gmail.com>
2018-07-19 00:59:31 +0200

    Deleted comments

--- cards.js
+++ cards.js
@@ -2,8 +2,7 @@ var cards = ['Diamond', 'Spade', 'Heart', 'Club'];
 var currentCard = 'Heart';
 
 while (currentCard !== 'Spade') {
-/*  console.log(currentCard) */ // this might make your console explode, LOL
-  
+  console.log(currentCard);
   var randomNumber = Math.floor(Math.random * 4);
   currentCard = cards[randomNumber];
 }
