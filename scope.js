4f911e0c4acc80734e905cdc76409cbab6dc0ec8
Samuel Rodríguez <espeon.elgatocosmico@gmail.com>
2018-07-18 23:14:54 +0200

    Create scope.js

---
+++ scope.js
@@ -0,0 +1,27 @@
+// wrong code due to scope error at line 13
+
+var laundryRoom = 'Basement';
+var mailRoom = 'Room 1A';
+
+function myApartment() {
+  var mailBoxNumber = 'Box 3'; // myApartment functional scope variable
+  laundryRoom = 'In-unit';
+  console.log('Mail box: ' + mailBoxNumber + ', Laundry:' + laundryRoom);
+}
+
+console.log('Laundry: ' + laundryRoom + ', Mail: ' + mailRoom);
+
+console.log(mailBoxNumber); // will return an error as mailBoxNumber isn't set on global scope
+
+// right code just by calling myApartment function and setting the previous console.log within it
+
+var laundryRoom = 'Basement';
+var mailRoom = 'Room 1A';
+
+function myApartment() {
+  var mailBoxNumber = 'Box 3'; // myApartment functional scope variable
+  laundryRoom = 'In-unit';
+  console.log('Laundry: ' + laundryRoom + ', Mail: ' + mailRoom);
+}
+
+myApartment();
