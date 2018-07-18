e0859222d8e06268ff4608be3c4a934746036b16
Samuel Rodríguez <espeon.elgatocosmico@gmail.com>
2018-07-19 00:29:14 +0200

    Create vacationCompare.js

---
+++ vacationCompare.js
@@ -0,0 +1,14 @@
+var myPlaces = ['Euskal Herria', 'Madrid', 'Beijing'];
+var friendPlaces = ['Vigo', 'Madrid', 'Berlin'];
+
+for(var i = 0; i < myPlaces.length; i++) {
+  console.log(myPlaces[i]);
+  for(var j = 0; j < friendPlaces.length; j++) {
+    console.log(friendPlaces[j]);
+    if (myPlaces[i] === friendPlaces[j] {
+      console.log(myPlaces[i]);
+    } else {
+    console.log('No places in common')
+    }
+  }
+}
